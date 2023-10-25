4f99435595ab3a1ed96246bc395b355a60a01c86
dongyongtao <dongyongtao@nucleisys.com>
2023-10-25 14:50:27 +0800

    Update the yaml file supporting to define a macro with parameters and return value
    - update the csi_ll_csr_access.h
    - #ifndef __ASSEMBLER__ is introduced
    
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- api/C/include/csi_ll_csr_access.h
+++ api/C/include/csi_ll_csr_access.h
@@ -7,11 +7,13 @@
  * generated file: DO NOT EDIT
  */
 
-#ifndef CSI_CSR_ACCESS_H
-#define CSI_CSR_ACCESS_H
+#ifndef CSI_LL_CSR_ACCESS_H
+#define CSI_LL_CSR_ACCESS_H
 
 #include "csi_types.h"
 #include <stdint.h>
+
+#ifndef __ASSEMBLER__
 
 /*
  * macros
@@ -20,95 +22,122 @@
 #define STRINGIFY(s)            __STR(s)
 #define __ASM                   __asm
 
+
 /*
  * Read the content of csr register to __v, then write content of val into csr
- * register, then return __v
+ * register, then return __v.
+ *
+ * @param csr: CSR macro definition
+ * @param val: value to store into the CSR register
+ * @return : the CSR register value before written
  */
 #define csi_csr_swap(csr, val)                                  \
     ({                                                          \
         rv_csr_t __v = (unsigned long)(val);                    \
         __ASM volatile("csrrw %0, " STRINGIFY(csr) ", %1"       \
-                     : "=r"(__v)                                \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                    : "=r"(__v)                                \
+                    : "rK"(__v)                                \
+                    : "memory");                               \
         __v;                                                    \
     })
 
 /*
- * Read the content of csr register to __v and return it
+ * Read the content of csr register to __v and return it.
+ *
+ * @param csr: CSR macro definition
+ * @return : the CSR register value
  */
 #define csi_csr_read(csr)                                       \
     ({                                                          \
         rv_csr_t __v;                                           \
         __ASM volatile("csrr %0, " STRINGIFY(csr)               \
-                     : "=r"(__v)                                \
-                     :                                          \
-                     : "memory");                               \
+                      : "=r"(__v)                                \
+                      :                                          \
+                      : "memory");                               \
         __v;                                                    \
     })
 
 /*
- * Write the content of val to csr register
+ * Write the content of val to csr register.
+ *
+ * @param csr: CSR macro definition
+ * @param val: value to store into the CSR register
  */
 #define csi_csr_write(csr, val)                                 \
     ({                                                          \
         rv_csr_t __v = (rv_csr_t)(val);                         \
         __ASM volatile("csrw " STRINGIFY(csr) ", %0"            \
-                     :                                          \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                      :                                          \
+                      : "rK"(__v)                                \
+                      : "memory");                               \
     })
 
 /*
  * Read the content of csr register to __v, then set csr register to be __v | val,
- * then return __v
+ * then return __v.
+ *
+ * @param csr: CSR macro definition
+ * @param val: Mask value to be used wih csrrs instruction
+ * @return : the CSR register value before written
  */
 #define csi_csr_read_set(csr, val)                              \
     ({                                                          \
         rv_csr_t __v = (rv_csr_t)(val);                         \
         __ASM volatile("csrrs %0, " STRINGIFY(csr) ", %1"       \
-                     : "=r"(__v)                                \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                    : "=r"(__v)                                \
+                    : "rK"(__v)                                \
+                    : "memory");                               \
         __v;                                                    \
     })
 
 /*
- * Set csr register to be csr_content | val
+ * Set csr register to be csr_content | val.
+ *
+ * @param csr: CSR macro definition
+ * @param val: Mask value to be used wih csrs instruction
  */
 #define csi_csr_set(csr, val)                                   \
     ({                                                          \
         rv_csr_t __v = (rv_csr_t)(val);                         \
         __ASM volatile("csrs " STRINGIFY(csr) ", %0"            \
-                     :                                          \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                    :                                          \
+                    : "rK"(__v)                                \
+                    : "memory");                               \
     })
 
 /*
  * Read the content of csr register to __v, then set csr register to be __v & ~val,
- * then return __v
+ * then return __v.
+ *
+ * @param csr: CSR macro definition
+ * @param val: Mask value to be used wih csrrc instruction
+ * @return : the CSR register value before written
  */
 #define csi_csr_read_clear(csr, val)                            \
     ({                                                          \
         rv_csr_t __v = (rv_csr_t)(val);                         \
         __ASM volatile("csrrc %0, " STRINGIFY(csr) ", %1"       \
-                     : "=r"(__v)                                \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                    : "=r"(__v)                                \
+                    : "rK"(__v)                                \
+                    : "memory");                               \
         __v;                                                    \
     })
 
 /*
  * Set csr register to be csr_content & ~val
+ *
+ * @param csr: CSR macro definition
+ * @param val: Mask value to be used wih csrc instruction
  */
 #define csi_csr_clear(csr, val)                                 \
     ({                                                          \
         rv_csr_t __v = (rv_csr_t)(val);                         \
         __ASM volatile("csrc " STRINGIFY(csr) ", %0"            \
-                     :                                          \
-                     : "rK"(__v)                                \
-                     : "memory");                               \
+                    :                                          \
+                    : "rK"(__v)                                \
+                    : "memory");                               \
     })
 
-#endif /* CSI_CSR_ACCESS_H */ 
+#endif // __ASSEMBLER__
+
+#endif /* CSI_LL_CSR_ACCESS_H */ 
